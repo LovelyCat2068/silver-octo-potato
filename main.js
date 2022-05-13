19c51c432e0018b16d67f1dd594e6f732c3ebada
LovelyCat2068 <76818025+LovelyCat2068@users.noreply.github.com>
2022-05-13 18:55:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	coin = loadSound("coin.wav");
+	game_over = loadSound("gameover.wav");
+	jump = loadSound("jump.wav");
+	kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	canvas.parent("canvas");
+	video = createCapure(VIDEO);
+	video.size(800,400);
+	video.parent("video_game");
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
